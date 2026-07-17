d5f6cf0951768d216fb08db047f453e84f3d7092
Analina Nielsen <analinanielsen@outlook.com>
2026-07-17 08:23:52 -0700

    I updated the home page with my student info.

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,13 @@ export default function Page() {
     <main className="flex min-h-screen flex-col p-6">
       {/* <div className={styles.shape} /> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
         {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
+      {/* Student info */}
+      <p className="mt-2 text-sm font-light text-gray-500">
+        Student: Analina Nielsen — WDD430 (BYU-Idaho)
+      </p>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           {/* <div
